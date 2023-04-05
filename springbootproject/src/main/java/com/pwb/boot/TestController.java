07e2a3059d724f8c008000fcb0b317770b0fc866
Debjyoti <Debjyoti@Debjyoti-Kittu>
2023-04-05 12:29:29 +0530

    ok

---
+++ springbootproject/src/main/java/com/pwb/boot/TestController.java
@@ -0,0 +1,14 @@
+package com.pwb.boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String firstHandler() {
+		return "just for testing";
+	}
+
+}
